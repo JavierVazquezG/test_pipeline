c4e0b4814cd91dd3969be8d9fa07d358e3fdd922
Javier Vazquez <javier.vazquez@amount.com>
2021-11-10 14:53:52 -0600

    Test

--- copyFiles.sh
+++ copyFiles.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 
 CURRENTDATE=`date +"%Y/%m/%d"`
 echo ${CURRENTDATE}
+cd ~/Reports
+LISTFILE='ls -l'
+echo ${LISTFILE}
 #aws s3 cp ~/Reports s3://amount-shared-cicd-security-reports.use2/${CURRENTDATE} --region us-east-2
