e1042db6b821af9ba9294ab91b5ce3ada260d838
Javier Vazquez <javier.vazquez@amount.com>
2021-11-11 09:52:09 -0600

    Listing the root directory of the agent

--- copyFiles.sh
+++ copyFiles.sh
@@ -5,6 +5,5 @@ set -euo pipefail
 CURRENTDATE=`date +"%Y/%m/%d"`
 echo ${CURRENTDATE}
 cd .
-LISTFILE='ls -l'
-echo ${LISTFILE}
+ls -l
 #aws s3 cp ~/Reports s3://amount-shared-cicd-security-reports.use2/${CURRENTDATE} --region us-east-2
