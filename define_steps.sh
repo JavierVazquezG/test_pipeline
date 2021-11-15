305f268d51f960ee921a5bb2eea148557feda679
Javier Vazquez <javier.vazquez@amount.com>
2021-11-15 12:50:38 -0600

    test1

--- define_steps.sh
+++ define_steps.sh
@@ -1,19 +1,16 @@
 #!/bin/bash -x
 
-name=$(hostname)
-echo $hostname
-
 cat << EOF
   - name: "Docker Image - Python Script" 
     agents:
-      name: "$name"
+      queue: eks-buildkite-agent
     label: ":docker::python::aws:"
     plugins:
       - docker-compose#v3.7.0:
           run: app
-  - wait
-  - name: "Copying Files"
-    agents:
-      name: "$name"
-    commands: 'copyFiles.sh'
 EOF
+#  - wait
+#  - name: "Copying Files"
+#    agents:
+#    commands: 'copyFiles.sh'
+#EOF
