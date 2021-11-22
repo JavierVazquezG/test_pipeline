743a38a9f1c1f23e0f3f1c9fc08152b5607a3a57
Javier Vazquez <javier.vazquez@amount.com>
2021-11-22 12:19:00 -0600

    test1

--- TFSec_part1.py
+++ TFSec_part1.py
@@ -26,40 +26,36 @@ def updateS3Bucket(S3Bucket, filename, key):
     s3.upload_file('/usr/src/app/'+filename, S3Bucket, key)
 
 if __name__ == '__main__':
-    print(boto3.client('sts').get_caller_identity()['Account'])
     
-    #os.system("mkdir Reports")
-    os.system("touch test.txt")
-    os.system("ls -lrt ~/" )
-    os.system("cd ~")
-    os.system("pwd")
-    #os.system("mv test.txt ~/usr/src/app/Reports")
-    updateS3Bucket(S3Bucket, "test.txt", "18")
+    
+    # os.system("touch test.txt")
+    # os.system("ls -lrt ~/" )
+    # os.system("cd ~")
+    # os.system("pwd")
+    # updateS3Bucket(S3Bucket, "test.txt", "18")
     
 
-    #for page in range(1,32):
-    #    response = requests.get('https://api.github.com/orgs/amount/repos?page='+str(page), headers=headers)
-    #    data = response.json()
+    for page in range(1,32):
+       response = requests.get('https://api.github.com/orgs/amount/repos?page='+str(page), headers=headers)
+       data = response.json()
 
-    #    for repo in data:
-    #        if(repo["language"] == 'HCL'):
-    #            date = datetime.datetime.now()
-    #            folder_name = date.strftime('%Y-%m-%d')
-    #            reposName = repo["name"]
-    #            ssh_clone_url = repo["ssh_url"]
-    #            path = os.getcwd()
-    #            if os.path.exists('cloneReposDirectory'):
-    #                shutil.rmtree('cloneReposDirectory')
-    #            os.mkdir('cloneReposDirectory')
-    #            os.chdir(path+'/cloneReposDirectory')
-    #            os.mkdir(reposName)
-    #            os.chdir(reposName)
-    #            git.Repo.clone_from(ssh_clone_url, '.')
-    #            if not (os.path.exists('~/Reports')):
-    #                os.system("mkdir ~/Reports")
-    #            os.system("tfsec . --format csv --out TFSec_Report_"+reposName+".csv")
-    #            os.system("mv TFSec_Report_"+reposName+".csv ~/Reports/")
-    #            file_Report = "TFSec_Report_"+reposName+".csv"
-    #            updateS3Bucket(S3Bucket, folder_name, file_Report)
-    #            os.chdir(path)
-    #            shutil.rmtree('cloneReposDirectory')
+       for repo in data:
+           if(repo["language"] == 'HCL'):
+               date = datetime.datetime.now()
+               folder_name = date.strftime('%Y-%m-%d')
+               reposName = repo["name"]
+               ssh_clone_url = repo["ssh_url"]
+               path = os.getcwd()
+               if os.path.exists('cloneReposDirectory'):
+                   shutil.rmtree('cloneReposDirectory')
+               os.mkdir('cloneReposDirectory')
+               os.chdir(path+'/cloneReposDirectory')
+               os.mkdir(reposName)
+               os.chdir(reposName)
+               git.Repo.clone_from(ssh_clone_url, '.')
+               os.system("tfsec . --format csv --out TFSec_Report_"+reposName+".csv")
+               os.system("mv TFSec_Report_"+reposName+".csv ~/")
+               file_Report = "TFSec_Report_"+reposName+".csv"
+               updateS3Bucket(S3Bucket, folder_name, file_Report)
+               os.chdir(path)
+               shutil.rmtree('cloneReposDirectory')
