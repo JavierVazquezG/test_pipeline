3aa8ea2275373a8fa51adcb27e7146cff958180c
Juan Reyes <jfr10v@amount.com>
2021-12-13 13:40:56 -0500

    test auth

--- TFSec_part1.py
+++ TFSec_part1.py
@@ -1,65 +1,20 @@
 #!/usr/bin/env python3.7
 
-from genericpath import exists
 import requests
-import os
-import shutil
-import git
-import boto3
-import botocore
-import datetime
-from datetime import date
+import json
 
-# Get environment variables
-S3Bucket = os.getenv('S3_BUCKET')
-TOKEN_API = os.getenv('GIT_API')
+username = 'jfr10v'
+token = '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'
 
-headers = {
-    'accept': 'application/vnd.github.vixen-preview+json',
-    'authorization': 'Bearer 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',
-    'content-type': 'application/json',
-}
+repos_url = 'https://api.github.com/orgs/amount/repos'
 
-#Setting the session of the boto3
-session = boto3.Session()
+# create a re-usable session object with the user creds in-built
+gh_session = requests.Session()
+gh_session.auth = (username, token)
 
-def updateS3Bucket(S3Bucket, filename, key):
-    s3 = session.client('s3')
-    s3.upload_file('/usr/src/app/'+filename, S3Bucket, key)
+# get the list of repos belonging to me
+repos = json.loads(gh_session.get(repos_url).text)
 
-if __name__ == '__main__':
-    
-    
-    # os.system("touch test.txt")
-    # os.system("ls -lrt ~/" )
-    # os.system("cd ~")
-    # os.system("pwd")
-    # updateS3Bucket(S3Bucket, "test.txt", "18")
-    
-
-    for page in range(1,32):
-       response = requests.get('https://api.github.com/orgs/amount/repos?page='+str(page)+'&per_page=100', headers=headers)
-       data = response.json()
-       
-       print (data)
-       
-       for repo in data:
-           if(repo["language"] == 'HCL'):
-               #date = datetime.datetime.now()
-               #folder_name = date.strftime('%Y-%m-%d')
-               reposName = repo["name"]
-               ssh_clone_url = repo["ssh_url"]
-               path = os.getcwd()
-               if os.path.exists('cloneReposDirectory'):
-                   shutil.rmtree('cloneReposDirectory')
-               os.mkdir('cloneReposDirectory')
-               os.chdir(path+'/cloneReposDirectory')
-               os.mkdir(reposName)
-               os.chdir(reposName)
-               git.Repo.clone_from(ssh_clone_url, '.')
-               os.system("tfsec . --format csv --out TFSec_Report_"+reposName+".csv")
-               os.system("mv TFSec_Report_"+reposName+".csv ~/")
-               file_Report = "TFSec_Report_"+reposName+".csv"
-               updateS3Bucket(S3Bucket, file_Report, "test1")
-               os.chdir(path)
-               shutil.rmtree('cloneReposDirectory')
+# print the repo names
+for repo in repos:
+    print (repo["name"])
