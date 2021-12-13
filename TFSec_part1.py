f4ea8200f8cb1f80744446a9fb7b8d4f2ac0f898
Juan Reyes <jfr10v@amount.com>
2021-12-13 14:32:31 -0500

    test2002

--- TFSec_part1.py
+++ TFSec_part1.py
@@ -3,19 +3,18 @@
 import requests
 import json
 import os
-token = os.getenv('GIT_API')
 username = 'jfr10v'
+token = os.getenv('GIT_API')
 
-repos_url = 'https://api.github.com/orgs/amount/repos'
 
 # create a re-usable session object with the user creds in-built
 gh_session = requests.Session()
 gh_session.auth = (username, token)
 
 # get the list of repos belonging to me
-repos = json.loads(gh_session.get(repos_url).text)
-
-# print the repo names
-for repo in repos:
-    print (repos)
-    print (repo["name"])
+for page in range(1,32):
+    repos_url = 'https://api.github.com/orgs/amount/repos?'+str(page)+'&per_page=100'
+    repos = json.loads(gh_session.get(repos_url).text)
+    for repo in repos:
+        print (repo["language"]=="HCL")
+   
